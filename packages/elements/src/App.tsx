a85da1e1425c0c2d41995c557ef2fc8e1b61b2fc
Rajiv Seelam <rajiv@betalectic.com>
2025-01-20 18:24:25 +0530

    feat: update @rcls/elements package version and add tailwindcss-scoped-preflight support

--- packages/elements/src/App.tsx
+++ packages/elements/src/App.tsx
@@ -1,11 +1,10 @@
-import "../lib/main.ts";
 import { Badge, Button } from "../lib/main.ts";
 
 export default function App() {
   return (
-    <>
+    <div>
       <Button>I am a button</Button>
       <Badge>I am a badge</Badge>
-    </>
+    </div>
   );
 }
--- packages/elements/tailwind.config.ts
+++ packages/elements/tailwind.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from "tailwindcss";
+import {
+  scopedPreflightStyles,
+  isolateInsideOfContainer,
+} from "tailwindcss-scoped-preflight";
 
 export default {
   prefix: "el-",
-  corePlugins: {
-    preflight: false,
-  },
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -13,5 +14,11 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    scopedPreflightStyles({
+      isolationStrategy: isolateInsideOfContainer(".twp", {
+        except: ".no-twp",
+      }),
+    }),
+  ],
 } satisfies Config;
