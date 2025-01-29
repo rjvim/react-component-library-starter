255cefc51c0e31799d1f9b3ca18a2715428f7e45
Rajiv Seelam <rajiv@betalectic.com>
2025-01-29 12:14:37 +0530

    feat: initialize elements-tw-v4 package with basic structure and components

---
+++ packages/elements-tw-v4/lib/Badge/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Badge(
+  props: {
+    children?: React.ReactNode;
+    className?: string;
+  } & React.HTMLAttributes<HTMLSpanElement>
+) {
+  return (
+    <span className="twp eln:inline-flex eln:items-center eln:rounded-md eln:bg-red-50 eln:px-2 eln:py-1 eln:text-xs eln:font-medium eln:text-red-600 eln:ring-1 eln:ring-inset eln:ring-red-500/10">
+      {JSON.stringify(props)}
+    </span>
+  );
+}
+
+export { Badge };
